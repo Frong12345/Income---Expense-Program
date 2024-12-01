1da2457683fbda8d9aee71028f6c8c82fb608a43
Frong12345 <frong_5057@hotmail.co.th>
2024-12-01 19:20:24 +0700

    Format Number

--- script.js
+++ script.js
@@ -25,11 +25,15 @@ function addDataToList(transactions){
     const symbol = transactions.amount < 0 ? '-':'+';
     const status = transactions.amount < 0 ? 'minus':'plus';
     const item = document.createElement('li');
+    result = formatNumber(Math.abs(transactions.amount));
     item.classList.add(status);
     // item.innerHTML = 'ค่าซ่อมรถ <span>- ฿400</span><button class="delete-btn">x</button>';
-    item.innerHTML = `${transactions.text}<span>${symbol}${Math.abs(transactions.amount)}</span><button class="delete-btn">x</button>`;
+    item.innerHTML = `${transactions.text}<span>${symbol}${result}</span><button class="delete-btn">x</button>`;
     list.appendChild(item)
     
+}
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 }
 function calculateMoney(){
     const amounts = transactions.map(transactions=>transactions.amount);
@@ -40,14 +44,14 @@ function calculateMoney(){
     // คำนวณรายจ่าย
     const expense = (amounts.filter(item=>item<0).reduce((result,item)=>(result+=item),0)*-1).toFixed(2);
     
+
+    
     
     //แสดงผลผ่านจอภาพ
-    balance.innerText = `฿${total}`;
-    money_plus.innerText = `฿${income}`;
-    money_minus.innerText = `฿${expense}`;
-    
-    
-    
+    balance.innerText = `฿` + formatNumber(total);
+    money_plus.innerText = `฿` + formatNumber(income);
+    money_minus.innerText = `฿` + formatNumber(expense);
+   
 }
 
 init();
